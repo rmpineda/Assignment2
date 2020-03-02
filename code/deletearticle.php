5ddeedb7dd11e58e84d12e347928c526e8818946
Remy Pineda <50272926+rmpineda@users.noreply.github.com>
2020-03-01 20:25:32 -0800

    Update deletearticle.php
    Added role-based access control enforcement and fixed broken access controls to prevent unauthorized users from performing authenticated user actions.

--- code/deletearticle.php
+++ code/deletearticle.php
@@ -2,12 +2,18 @@
 session_start();
 include("config.php");
 include("lib/db.php");
+include("lib/auth.php");
 
-$aid = $_GET['aid'];
-#echo "aid=".$aid."<br>";
-$result = delete_article($dbconn, $aid);
-#echo "result=".$result."<br>";
-# Check result
-header("Location: /admin.php");
+$current_author = $_SESSION['author'];
 
+if ($_SESSION['authenticated'] == True && ($_SESSION['username'] == "admin" || $_SESSION['username'] == $current_author)) {
+	$aid = $_GET['aid'];
+	#echo "aid=".$aid."<br>";
+	$result = delete_article($dbconn, $aid);
+	#echo "result=".$result."<br>";
+	# Check result
+	header("Location: /admin.php");
+} else {
+	echo "Unable to delete.";
+}
 ?>
